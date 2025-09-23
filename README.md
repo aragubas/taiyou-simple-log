04fbaafbcd21c31d095fe164b3f2a7d2f96e696b
aragubas <vaiogames18@gmail.com>
2025-09-23 10:51:49 -0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,68 @@
+# Taiyou Simple Log
+A simple, configurable and elegant logging library for Godot 4.* C# projects!
+
+By using C#'s compiler definitions, you can configure TaiyouSimpleLog to do exactly what you want without any additional runtime costs! 
+
+TaiyouSimpleLog supports projects with and without Reflection (useful for Web/iOS/Android builds)
+
+## Example 
+By default, TaiyouLog is configured with Reflection enabled, Color Messages and PrintRich disabled 
+
+```c#
+Log.Debug(""); // Logs message into OS terminal and Godot's built in output
+Log.Warning(""); // Logs message into Godot's built in debugger (if PrintRich is disabled) and OS terminal
+Log.Error(""); // Logs message into Godot's built in debugger (if PrintRich is disabled) and OS terminal
+```
+
+## Customization
+TaiyouSimpleLog has 5 compiler definitions that can be set:
+1. TAIYOULOG_NO_REFLECTION
+1. TAIYOULOG_NO_COLOR_MESSAGE
+1. TAIYOULOG_PRINTRICH
+1. TAIYOULOG_WARNING_PRINTRICH
+1. TAIYOULOG_ERROR_PRINTRICH 
+
+### TAIYOULOG_NO_REFLECTION
+If set, any code that uses reflection will be disabled. Since TaiyouSimpleLog fetches the classname and methodname using Reflection, it will require you to manually set the ClassName and MethodName using the ``methodName`` and ``className`` parameters.
+
+### TAIYOULOG_NO_COLOR_MESSAGE
+If set, disables coloring the message segment from log output
+
+### TAIYOULOG_PRINTRICH 
+If set, enables PrintRich for Warnings and Errors. PrintRich will make errors and warnings not be logged into Godot's built in debugger, and instead on the Output window/OS Terminal.
+
+### TAIYOULOG_(WARNING/ERROR)_PRINTRICH
+If set, enables PrintRich for either Warning or Error, available as two compiler definitions
+1. TAIYOULOG_WARNING_PRINTRICH
+1. TAIYOULOG_ERROR_PRINTRICH
+---
+### Changing colors
+You can also change the color set for Debug, Error and Warning (if PrintRich is enabled) by changing the respective variables in the ``Log.cs`` class
+1. DebugColor
+1. WarningColor
+1. ErrorColor
+
+Methods for restoring the default color is also available
+1. ResetDebugColor()
+1. ResetWarningColor()
+1. ResetErrorColor()
+
+
+### Example
+Changing the color of a debug output to blue, and then restoring it back
+```c#
+Log.DebugColor = "blue"; // Must be color codes accepted by Godot's PrintRich method
+Log.Debug("Debug! but in blue");
+Log.ResetDebugColor(); // Resets to the default color, hardcoded in the class
+```
+
+# Donations 
+If you like what you see and wants to help us develop more useful libraries, check out my funding platforms below!
+
+Kofi (https://ko-fi.com/aragubas)
+
+
+# License
+This project has been licensed with the BSD with Attribution license. 
+
+More information at [LICENSE.txt](LICENSE.txt) 
---
+++ src/Log.cs
@@ -0,0 +1,189 @@
+/*
+  License with "BSD with Attribution" (https://fedoraproject.org/wiki/Licensing/BSD_with_Attribution)
+  by Aragubas/Harley Sky Saphri at harleyskysaphri@gmail.com or https://aragubas.com
+*/
+
+#if !TAIYOULOG_NO_REFLECTION
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+#endif
+using Godot;
+
+namespace TaiyouSimpleLog;
+
+public static class Log {
+  static readonly string s_DefaultDebugColor = "dimgray";
+  static readonly string s_DefaultWarningColor = "orange";
+  static readonly string s_DefaultErrorColor = "red";
+
+  public static string DebugColor = s_DefaultDebugColor;
+  public static string WarningColor = s_DefaultWarningColor;
+  public static string ErrorColor = s_DefaultErrorColor;
+
+  /// <summary>
+  /// Resets DebugColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetDebugColor() {
+    DebugColor = s_DefaultDebugColor;
+  }
+
+  /// <summary>
+  /// Resets WarningColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetWarningColor() {
+    WarningColor = s_DefaultWarningColor;
+  }
+
+  /// <summary>
+  /// Resets ErrorsColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetErrorColor() {
+    ErrorColor = s_DefaultErrorColor;
+  }
+
+  /// <summary>
+  /// Logs message into OS terminal 
+  /// </summary>
+  /// <param name="message">Message to log</param>
+  /// <param name="includeNamespace">If true, includes the whole namespace instead of just the name of the function</param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+#if !TAIYOULOG_NO_REFLECTION
+  [StackTraceHidden]
+#endif
+  public static void Debug(string message, bool includeNamespace = false, string methodName = "", string className = "") {
+#if !TAIYOULOG_NO_REFLECTION
+    // Colors colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color={DebugColor}]{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)};[/color] {message}";
+#else
+    string printMessage = $"[color={DebugColor}]{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message} [/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+#else
+    // Colors colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color={DebugColor}]{MethodHeader(className, methodName)};[/color] {message}";
+#else
+    string printMessage = $"[color={DebugColor}]{MethodHeader(className, methodName)}; {message} [/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+#endif
+  }
+
+  /// <summary>
+  /// Logs message into Godot's built in debugger (if TAIYOULOG_PRINTRICH or TAIYOULOG_WARNING_PRINTRICH is <b>not</b> defined) and into OS terminal
+  /// </summary>
+  /// <param name="message"></param>
+  /// <param name="includeNamespace"></param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+#if !TAIYOULOG_NO_REFLECTION
+  [StackTraceHidden]
+#endif
+  public static void Warning(string message, bool includeNamespace = false, string methodName = "", string className = "") {
+#if !TAIYOULOG_NO_REFLECTION
+    //  If NO_REFLECTION is NOT set (default behavior)
+#if TAIYOULOG_WARNING_PRINTRICH || TAIYOULOG_PRINTRICH
+
+    // Colors colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color={WarningColor}] WARN {MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)};[/color] {message}";
+#else
+    string printMessage = $"[color={WarningColor}] WARN {MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message} [/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+#else
+    GD.PushWarning($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
+#endif
+
+#else
+    //  If NO_REFLECTION is set
+#if TAIYOULOG_WARNING_PRINTRICH || TAIYOULOG_PRINTRICH
+
+    // Colors! Colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color={WarningColor}] WARN {MethodHeader(className, methodName)};[/color] {message}";
+#else
+    string printMessage = $"[color={WarningColor}] WARN {MethodHeader(className, methodName)}; {message}[/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+#else
+    GD.PushWarning($"{MethodHeader(className, methodName)}; {message}");
+#endif
+#endif
+  }
+
+  /// <summary>
+  /// Logs message into standard error output (if TAIYOULOG_PRINTRICH or TAIYOULOG_ERROR_PRINTRICH is <b>not</b> defined) and into OS terminal
+  /// </summary>
+  /// <param name="message"></param>
+  /// <param name="includeNamespace"></param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+ #if !TAIYOULOG_NO_REFLECTION
+  [StackTraceHidden]
+#endif
+  public static void Error(string message, bool includeNamespace = false, string methodName = "", string className = "") {
+#if !TAIYOULOG_NO_REFLECTION
+    //  If NO_REFLECTION is NOT set (default behavior)
+#if TAIYOULOG_ERROR_PRINTRICH || TAIYOULOG_PRINTRICH
+
+    // Colors colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color=${ErrorColor}]ERROR {MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)};[/color] {message}";
+#else
+    string printMessage = $"[color={ErrorColor}]ERROR {MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message} [/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+
+#else
+    GD.PushError($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
+#endif
+
+#else
+    //  If NO_REFLECTION is set
+#if TAIYOULOG_ERROR_PRINTRICH || TAIYOULOG_PRINTRICH
+
+    // Colors! Colors!
+#if TAIYOULOG_NO_COLOR_MESSAGE
+    string printMessage = $"[color={ErrorColor}]ERROR {MethodHeader(className, methodName)};[/color] {message}";
+#else
+    string printMessage = $"[color={ErrorColor}]ERROR {MethodHeader(className, methodName)}; {message}[/color]";
+#endif
+
+    GD.PrintRich(printMessage);
+#else
+    GD.PushError($"{MethodHeader(className, methodName)}; {message}");
+#endif
+#endif
+  }
+
+#if !TAIYOULOG_NO_REFLECTION
+  /// <summary>
+  /// Gets the name of the function called before this one and optionally the namespace
+  /// </summary>
+  /// <param name="methodBase">MethodBase reflection parameter</param>
+  /// <param name="includeNamespace">If true, includes the name of the calling namespace</param>
+  /// <returns></returns>
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static string MethodHeader(MethodBase methodBase, bool includeNamespace = false) {
+    return $"{(includeNamespace ? methodBase.DeclaringType : methodBase.DeclaringType.Name)}::{methodBase.Name}";
+  }
+#else
+  /// <summary>
+  /// Returns a neatly formatted string with the className and methodName as if Reflection was available
+  /// </summary>
+  /// <param name="className">Name of the calling class (without namespace)</param>
+  /// <param name="methodName">Name of the calling method</param>
+  /// <returns>A neatly formatted string with className and methodName</returns>
+  public static string MethodHeader(string className, string methodName)
+    => $"{className}::{methodName}";
+#endif
+}
