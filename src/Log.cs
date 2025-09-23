b06424aadaccdfcb4978d2ca1848f33d965a5867
aragubas <vaiogames18@gmail.com>
2025-09-23 12:07:44 -0300

    Add EditorConfig and re-format Log.cs

--- src/Log.cs
+++ src/Log.cs
@@ -13,61 +13,56 @@ using Godot;
 
 namespace TaiyouSimpleLog;
 
-public static class Log
-{
-    static readonly string s_DefaultDebugColor = "dimgray";
-    static readonly string s_DefaultWarningColor = "orange";
-    static readonly string s_DefaultErrorColor = "red";
+public static class Log {
+  static readonly string s_DefaultDebugColor = "dimgray";
+  static readonly string s_DefaultWarningColor = "orange";
+  static readonly string s_DefaultErrorColor = "red";
 
-    public static string DebugColor = s_DefaultDebugColor;
-    public static string WarningColor = s_DefaultWarningColor;
-    public static string ErrorColor = s_DefaultErrorColor;
+  public static string DebugColor = s_DefaultDebugColor;
+  public static string WarningColor = s_DefaultWarningColor;
+  public static string ErrorColor = s_DefaultErrorColor;
 
-    /// <summary>
-    /// Resets DebugColor to it's default, hardcoded value
-    /// </summary>
-    public static void ResetDebugColor()
-    {
-        DebugColor = s_DefaultDebugColor;
-    }
+  /// <summary>
+  /// Resets DebugColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetDebugColor() {
+    DebugColor = s_DefaultDebugColor;
+  }
 
-    /// <summary>
-    /// Resets WarningColor to it's default, hardcoded value
-    /// </summary>
-    public static void ResetWarningColor()
-    {
-        WarningColor = s_DefaultWarningColor;
-    }
+  /// <summary>
+  /// Resets WarningColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetWarningColor() {
+    WarningColor = s_DefaultWarningColor;
+  }
 
-    /// <summary>
-    /// Resets ErrorsColor to it's default, hardcoded value
-    /// </summary>
-    public static void ResetErrorColor()
-    {
-        ErrorColor = s_DefaultErrorColor;
-    }
+  /// <summary>
+  /// Resets ErrorsColor to it's default, hardcoded value
+  /// </summary>
+  public static void ResetErrorColor() {
+    ErrorColor = s_DefaultErrorColor;
+  }
 
-    /// <summary>
-    /// Logs message into OS terminal 
-    /// </summary>
-    /// <param name="message">Message to log</param>
-    /// <param name="includeNamespace">If true, includes the whole namespace instead of just the name of the function</param>
-    /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
-    /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+  /// <summary>
+  /// Logs message into OS terminal 
+  /// </summary>
+  /// <param name="message">Message to log</param>
+  /// <param name="includeNamespace">If true, includes the whole namespace instead of just the name of the function</param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
 #if !TAIYOULOG_NO_REFLECTION
-    [StackTraceHidden]
+  [StackTraceHidden]
 #endif
-    public static void Debug(string message, bool includeNamespace = false, string methodName = "", string className = "")
-    {
+  public static void Debug(string message, bool includeNamespace = false, string methodName = "", string className = "") {
 #if !TAIYOULOG_NO_REFLECTION
-        // Colors colors!
+    // Colors colors!
 #if TAIYOULOG_NO_COLOR_MESSAGE
     string printMessage = $"[color={DebugColor}]{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)};[/color] {message}";
 #else
-        string printMessage = $"[color={DebugColor}]{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message} [/color]";
+    string printMessage = $"[color={DebugColor}]{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message} [/color]";
 #endif
 
-        GD.PrintRich(printMessage);
+    GD.PrintRich(printMessage);
 #else
     // Colors colors!
 #if TAIYOULOG_NO_COLOR_MESSAGE
@@ -78,22 +73,21 @@ public static class Log
 
     GD.PrintRich(printMessage);
 #endif
-    }
+  }
 
-    /// <summary>
-    /// Logs message into Godot's built in debugger (if TAIYOULOG_PRINTRICH or TAIYOULOG_WARNING_PRINTRICH is <b>not</b> defined) and into OS terminal
-    /// </summary>
-    /// <param name="message"></param>
-    /// <param name="includeNamespace"></param>
-    /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
-    /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+  /// <summary>
+  /// Logs message into Godot's built in debugger (if TAIYOULOG_PRINTRICH or TAIYOULOG_WARNING_PRINTRICH is <b>not</b> defined) and into OS terminal
+  /// </summary>
+  /// <param name="message"></param>
+  /// <param name="includeNamespace"></param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
 #if !TAIYOULOG_NO_REFLECTION
-    [StackTraceHidden]
+  [StackTraceHidden]
 #endif
-    public static void Warning(string message, bool includeNamespace = false, string methodName = "", string className = "")
-    {
+  public static void Warning(string message, bool includeNamespace = false, string methodName = "", string className = "") {
 #if !TAIYOULOG_NO_REFLECTION
-        //  If NO_REFLECTION is NOT set (default behavior)
+    //  If NO_REFLECTION is NOT set (default behavior)
 #if TAIYOULOG_WARNING_PRINTRICH || TAIYOULOG_PRINTRICH
 
     // Colors colors!
@@ -105,7 +99,7 @@ public static class Log
 
     GD.PrintRich(printMessage);
 #else
-        GD.PushWarning($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
+    GD.PushWarning($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
 #endif
 
 #else
@@ -124,22 +118,21 @@ public static class Log
     GD.PushWarning($"{MethodHeader(className, methodName)}; {message}");
 #endif
 #endif
-    }
+  }
 
-    /// <summary>
-    /// Logs message into standard error output (if TAIYOULOG_PRINTRICH or TAIYOULOG_ERROR_PRINTRICH is <b>not</b> defined) and into OS terminal
-    /// </summary>
-    /// <param name="message"></param>
-    /// <param name="includeNamespace"></param>
-    /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
-    /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
+  /// <summary>
+  /// Logs message into standard error output (if TAIYOULOG_PRINTRICH or TAIYOULOG_ERROR_PRINTRICH is <b>not</b> defined) and into OS terminal
+  /// </summary>
+  /// <param name="message"></param>
+  /// <param name="includeNamespace"></param>
+  /// <param name="methodName">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling method</param> 
+  /// <param name="className">(Only used if TAIYOULOG_NO_REFLECTION <b>is</b> defined) The name of the calling class</param>
 #if !TAIYOULOG_NO_REFLECTION
-    [StackTraceHidden]
+  [StackTraceHidden]
 #endif
-    public static void Error(string message, bool includeNamespace = false, string methodName = "", string className = "")
-    {
+  public static void Error(string message, bool includeNamespace = false, string methodName = "", string className = "") {
 #if !TAIYOULOG_NO_REFLECTION
-        //  If NO_REFLECTION is NOT set (default behavior)
+    //  If NO_REFLECTION is NOT set (default behavior)
 #if TAIYOULOG_ERROR_PRINTRICH || TAIYOULOG_PRINTRICH
 
     // Colors colors!
@@ -152,7 +145,7 @@ public static class Log
     GD.PrintRich(printMessage);
 
 #else
-        GD.PushError($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
+    GD.PushError($"{MethodHeader(new StackFrame(1).GetMethod(), includeNamespace)}; {message}");
 #endif
 
 #else
@@ -171,28 +164,26 @@ public static class Log
     GD.PushError($"{MethodHeader(className, methodName)}; {message}");
 #endif
 #endif
-    }
+  }
 
 #if !TAIYOULOG_NO_REFLECTION
-    /// <summary>
-    /// Gets the name of the function called before this one and optionally the namespace
-    /// </summary>
-    /// <param name="methodBase">MethodBase reflection parameter</param>
-    /// <param name="includeNamespace">If true, includes the name of the calling namespace</param>
-    /// <returns></returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string MethodHeader(MethodBase? methodBase, bool includeNamespace = false)
-    {
-        if (methodBase is MethodBase method)
-        {
-            string className = "(error)";
-            // Check if declaring type isn't null
-            if (methodBase.DeclaringType is Type type)
-                className = includeNamespace ? type.ToString() : type.Name;
-            return $"{className}::{methodBase.Name}";
-        }
-        return "(error, method base is null)";
+  /// <summary>
+  /// Gets the name of the function called before this one and optionally the namespace
+  /// </summary>
+  /// <param name="methodBase">MethodBase reflection parameter</param>
+  /// <param name="includeNamespace">If true, includes the name of the calling namespace</param>
+  /// <returns></returns>
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static string MethodHeader(MethodBase? methodBase, bool includeNamespace = false) {
+    if (methodBase is MethodBase method) {
+      string className = "(error)";
+      // Check if declaring type isn't null
+      if (methodBase.DeclaringType is Type type)
+        className = includeNamespace ? type.ToString() : type.Name;
+      return $"{className}::{methodBase.Name}";
     }
+    return "(error, method base is null)";
+  }
 #else
   /// <summary>
   /// Returns a neatly formatted string with the className and methodName as if Reflection was available
